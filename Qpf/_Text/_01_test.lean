bd0f24303ae336b12934501b3da699ac2f08ec19
Alex Keizer <alex@keizer.dev>
2022-11-20 12:39:36 +0100

    WIP changes to examples/text

--- Qpf/_Text/_01_test.lean
+++ Qpf/_Text/_01_test.lean
@@ -2,7 +2,7 @@ import Qpf
 import Qpf.Macro
 
 /-!
-  The ```inductive``` keyword encompasses three concepts: sums, products, and recursion.
+  The `inductive` keyword encompasses three concepts: sums, products, and recursion.
   Let's forget about the latter for a second, and look at a non-recursive type
 -/
 inductive Foo (α β γ : Type)
@@ -14,7 +14,7 @@ inductive Foo (α β γ : Type)
 -- | qux : γ → Foo' α β γ
 
 /-!
-  We can replace the two arguments of the ``bar`` constructor by their product.
+  We can replace the two arguments of the `bar` constructor by their product.
 -/
 
 inductive Foo₂ (α β γ : Type)
@@ -29,7 +29,7 @@ def Foo₃ (α β γ : Type) :=
   (α × β) ⊕ γ 
 
 /-!
-  We can make ``Foo₃`` a bit more ergonomic by defining constructor functions
+  We can make `Foo₃` a bit more ergonomic by defining constructor functions
 -/
 def Foo₃.bar {α β γ : Type} : 
   α → β → Foo₃ α β γ :=
