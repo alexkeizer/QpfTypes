ebf3f4b432791d85122aeddb4f9e98fb4a76c6ae
Alex Keizer <alex@keizer.dev>
2022-11-21 13:29:41 +0100

    Finish data command!!!!

--- Qpf/Macro/Data/ElabData.lean
+++ Qpf/Macro/Data/ElabData.lean
@@ -14,6 +14,9 @@ import Qpf.Macro.Data.MkQpf
 import Qpf.Macro.Data.Replace
 import Qpf.Macro.Data.Count
 import Qpf.Macro.Common
+import Qpf.Macro.Comp
+
+import Qpf.Qpf.Multivariate.Constructions.Fix
 
 open Lean Meta Elab.Command
 open Elab (Modifiers elabModifiers)
@@ -23,7 +26,7 @@ namespace Data.Command
 
 /-!
   ## Parser
-  for `data` declarations
+  for `data` and `codata` declarations
 -/
 section
   open Lean.Parser Lean.Parser.Command
@@ -34,9 +37,19 @@ section
                         >> many ctor 
                         >> optDeriving
 
+  def codataDecl : Parser
+    := leading_parser "codata " >> declId  >> optDeclSig  
+                        >> Parser.optional  (symbol " :=" <|> " where") 
+                        >> many ctor 
+                        >> optDeriving
+
   @[commandParser]
   def data : Parser
     := leading_parser declModifiers false >> dataDecl
+
+  @[commandParser]
+  def codata : Parser
+    := leading_parser declModifiers false >> codataDecl
 end
 
 /-!
@@ -44,10 +57,6 @@ end
 -/
 open Internals
 open Elab.Term (TermElabM)
-
-section
-
-
 
 def Name.replacePrefix (pref new_pref : Name) : Name → Name
   | Name.anonymous => Name.anonymous
@@ -97,14 +106,17 @@ def mkHeadT (view : InductiveView) : CommandElabM Name := do
       : CtorView
     } 
 
+  -- let type ← `(Type $(mkIdent `u))
+
+  -- TODO: make `HeadT` universe polymorphic
   let view := {
     ctors, declId, declName, shortDeclName,
     binders         := view.binders.setArgs #[]
-    levelNames      := []
+    levelNames      := view.levelNames
 
     ref             := view.ref            
     modifiers       := view.modifiers      
-    type?           := view.type?          
+    type?           := view.type?
     
     derivingClasses := view.derivingClasses
     : InductiveView
@@ -113,8 +125,22 @@ def mkHeadT (view : InductiveView) : CommandElabM Name := do
   elabInductiveViews #[view]
   pure declName
 
-#check Array
-#check List.range
+
+open Parser in
+private def matchAltsOfArray (matchAlts : Array Syntax) : Syntax :=
+  mkNode ``Term.matchAlts #[mkNullNode matchAlts]
+
+
+open Parser in
+/--
+  Wraps an array of `matchAltExpr` syntax objects into a single `Command.declValEqns` node, for
+  use in inductive definitions
+-/
+private def declValEqnsOfMatchAltArray (matchAlts : Array Syntax) : Syntax :=
+  let body := matchAltsOfArray matchAlts
+  let body := mkNode ``Term.matchAltsWhereDecls #[body, mkNullNode]
+  mkNode ``Command.declValEqns #[body]
+
 
 open Parser Parser.Term Parser.Command in
 def mkChildT (view : InductiveView) (r : Replace) (headTName : Name) : CommandElabM Name := do  
@@ -124,7 +150,7 @@ def mkChildT (view : InductiveView) (r : Replace) (headTName : Name) : CommandEl
   let declId := mkNode ``Command.declId #[mkIdent declName, mkNullNode]
   let shortDeclName := Name.mkSimple suffix
 
-  let arity := r.expr.length
+  let arity := r.arity
   let target_type := Syntax.mkApp (mkIdent ``TypeVec) #[quote arity]
 
   let binderIdents := r.getBinderIdents
@@ -134,14 +160,11 @@ def mkChildT (view : InductiveView) (r : Replace) (headTName : Name) : CommandEl
 
     let counts := countVarOccurences r ctor.type?
     let counts := counts.map fun n => Syntax.mkApp PFin2 #[quote n]
+    let counts := counts.reverse
 
     `(matchAltExpr| | $head => (##[ $counts,* ]))
 
-  -- Wrap the match arms in the appropriate syntax category, so it can be used in a `def` command
-  let body := mkNode ``Term.matchAlts #[mkNullNode matchAlts]
-  let body := mkNode ``Term.matchAltsWhereDecls #[body, mkNullNode]
-  let body := mkNode ``Command.declValEqns #[body]
-
+  let body := declValEqnsOfMatchAltArray matchAlts
   let headT := mkIdent headTName
 
   
@@ -150,30 +173,84 @@ def mkChildT (view : InductiveView) (r : Replace) (headTName : Name) : CommandEl
     def $declId : $headT → $target_type
       $body:declValEqns
   )
-  dbg_trace "{cmd}"
   elabCommand cmd
 
   pure declName
 
 
+-- structure CtorArgs where
+--   (all : Array Syntax)
+--   (byType : Array (Array Syntax))
+
+-- private def CtorArgs.ofType (acc : CtorArgs) : Syntax → CtorArgs
+
+-- private def CtorArgs.ofCtor (ctor : CtorView) (r : Replace) : CtorArgs :=
+--   let acc := ⟨
+--     Array.range 
+--   ⟩
+
+
+open Parser.Term in
+def mkQpf (shapeView : InductiveView) (headT childT P : Name) (arity : Nat) : CommandElabM Unit := do
+  let shape := shapeView.declName
+  let qpf_ := mkIdent $ Name.mkStr shape "qpf"
+
+  let P := mkIdent P
+  let Shape := mkIdent shape
+
+  let n_ctors := shapeView.ctors.size;
+
+  let box := matchAltsOfArray <|<- shapeView.ctors.mapM fun ctor => do
+    `(matchAltExpr| | _ => _)
+
+  let cmd ← `(
+    instance $qpf_ : $(mkIdent ``MvQpf) (@$(mkIdent ``TypeFun.ofCurried) $(quote arity) $Shape) := 
+      let box   : ∀{α}, $Shape α → $(P).Obj α :=
+          fun x => match x with
+            $box:matchAlts
+
+      let unbox : ∀{α}, $(P).Obj α → $Shape α := _
+      
+      $(mkIdent ``MvQpf.ofPolynomial) $P
+  )
+  elabCommand cmd
+
+  pure ()
+
+
+
+
+
+
+
+
+
+structure MkShapeResult where
+  (r : Replace)
+  (shape : Name)
+  (P : Name)
 
 open Parser in
-def mkShape (view: InductiveView) : CommandElabM Unit := do
+def mkShape (view: InductiveView) : CommandElabM MkShapeResult := do
   -- If the original declId was `MyType`, we want to register the shape type under `MyType.Shape`
   let suffix := "Shape"
   let declName := Name.mkStr view.declName suffix
   let declId := mkNode ``Command.declId #[mkIdent declName, mkNullNode]
   let shortDeclName := Name.mkSimple suffix
 
-  let headT := mkIdent $ Name.mkStr view.declName "HeadT"
-
 
   -- Extract the "shape" functors constructors
-  let (ctors, r) ← Replace.run (Replace.shapeOfCtors (mkIdent shortDeclName) view.ctors)
+  let origArgs    := Macro.getBinderIdents view.binders.getArgs
+  let origApp     := Syntax.mkApp (mkIdent view.shortDeclName) origArgs
+
+  dbg_trace "origApp := {origApp}"
+
+  let shapeIdent  := mkIdent shortDeclName
+  let (ctors, r) ← Replace.run origApp shapeIdent (Replace.shapeOfCtors view.ctors)
   let ctors := ctors.map (CtorView.declReplacePrefix view.declName declName)
 
 
-  -- Assemble it back together, into the shape functor
+  -- Assemble it back together, into the shape inductive type
   let binders ← r.getBinders  
   let view := {
     ctors, declId, declName, shortDeclName,
@@ -192,77 +269,188 @@ def mkShape (view: InductiveView) : CommandElabM Unit := do
   let headTName ← mkHeadT view
   let childTName ← mkChildT view r headTName
 
+  let declNameP := Name.mkStr declName "P"
+  let u ← `(ident| u) -- use a hygienic id for the universe
+  let declIdP := mkNode ``Command.declId #[mkIdent declNameP, mkNullNode 
+    -- #[ TODO: make this universe polymorphic
+    --   mkAtom ".{",
+    --   mkNullNode #[u],
+    --   mkAtom "}"
+    -- ]
+  ]
+
+  let childTId := mkIdent childTName
+
   elabCommand <|<- `(
-    def $(mkIdent $ Name.mkStr declName "P") := 
-      $(mkIdent ``MvPFunctor.mk) $(mkIdent headTName) $(mkIdent childTName)
+    def $declIdP := 
+      $(mkIdent ``MvPFunctor.mk):ident.{0} $(mkIdent headTName) $childTId
   )
 
+  pure ⟨r, declName, declNameP⟩
+
+
+
+
+open Parser Macro.Comp in
+def mkBase (view : InductiveView) : CommandElabM Syntax := do
+  let declId := mkIdent $ Name.mkStr view.declName "Base"
   
+  let ⟨r, shape, P⟩ ← mkShape view
+  let recurse := mkIdent r.recurse
+  let recurseBind := mkNode ``Term.simpleBinder #[mkNullNode #[recurse], mkNullNode]
+
+  let binders := view.binders
+  let binders := binders.setArgs (binders.getArgs.push recurseBind)
+  let args := r.expr.toArray.push recurse
+
+  let target ← `(
+    ($(mkIdent ``TypeFun.curried) ($(mkIdent ``MvPFunctor.Obj) $(mkIdent P))) $args*
+  )
+  dbg_trace "\n{target}\n"
+  elabQpfCommand declId binders none target  
+
+  pure declId
 
 
 
 
+def mkAuxConstructions (view : InductiveView) (internal : Syntax) : CommandElabM Unit := do
+  let cmd ← `(
+    def $(view.declId) := $(mkIdent ``TypeFun.curried) $internal
+  )
+  elabCommand cmd
 
 
 
-
-open Parser.Term Parser in
 /-- Top-level elaboration -/
 @[commandElab «data»]
 def elabData : CommandElab := fun stx => do 
   let modifiers ← elabModifiers stx[0]
   let decl := stx[1]
-  let mut view ← inductiveSyntaxToView modifiers decl
+  let view ← inductiveSyntaxToView modifiers decl
 
-  -- let (liveBinders, deadBinders) ← splitLiveAndDeadBinders view.binders.getArgs
-  -- dbg_trace "liveBinders: {liveBinders}"def MyList.Shape.ChildT : MyList.Shape.HeadT → Nat
---   | MyList.Shape.HeadT.nil  => 1
---   | MyList.Shape.HeadT.cons => 1
-  -- let newBinders ←
-  --   if liveBinders.size = 0 then
-  --     pure deadBinders
-  --   else
-  --     let liveBinders ← `(bracketedBinder| ( $liveBinders* : Type _ ))
-  --     pure <| deadBinders.push liveBinders
 
-  mkShape view
+  let base ← mkBase view
+
+  let internal := mkIdent $ Name.mkStr view.declName "Internal"
+  let cmd ← `(
+    abbrev $internal := $(mkIdent ``MvQpf.Fix) ($(mkIdent ``TypeFun.ofCurried)  $base)
+  ) 
+  elabCommand cmd
+
+  mkAuxConstructions view internal
   pure ()
 
-end
+/-- Top-level elaboration -/
+@[commandElab «codata»]
+def elabCodata : CommandElab := fun stx => do 
+  let modifiers ← elabModifiers stx[0]
+  let decl := stx[1]
+  let view ← inductiveSyntaxToView modifiers decl
+
+
+  let base ← mkBase view
+
+  let internal := mkIdent $ Name.mkStr view.declName "Internal"
+  let cmd ← `(
+    abbrev $internal := $(mkIdent ``MvQpf.Cofix) ($(mkIdent ``TypeFun.ofCurried)  $base)
+  ) 
+  elabCommand cmd
+
+  mkAuxConstructions view internal  
+  pure ()
+
 end Data.Command
 
-
-
+set_option pp.rawOnError true
 
 data MyList α β where
-  | nil : β → β → MyList α β
+  | nil : β → MyList α β
   | cons : α → MyList α β → MyList α β
 
+#check MyList Nat Int
+
+#print MyList.Internal
+
+def MyList.nil {α β} (b : β) : MyList α β
+  := MvQpf.Fix.mk ⟨MyList.Shape.HeadT.nil, fun i j => match i with
+      | 0 => Fin2.elim0 
+              (C := fun _ => _)
+              $ cast (by simp[Shape.P, Shape.ChildT, Vec.append1]) j
+      | 1 => b
+      | 2 => Fin2.elim0 
+              (C := fun _ => _)
+              $ cast (by simp[Shape.P, Shape.ChildT, Vec.append1]) j
+  ⟩
+
+def MyList.cons {α β} (a : α) (as : MyList α β) : MyList α β
+  := MvQpf.Fix.mk ⟨MyList.Shape.HeadT.cons, fun i j => match i with
+      | 0 => as
+      | 1 => Fin2.elim0 
+              (C := fun _ => _)
+              $ cast (by simp[Shape.P, Shape.ChildT, Vec.append1]) j
+      | 2 => a
+  ⟩
+
+#check (MvQpf.Fix.mk (F:=TypeFun.ofCurried MyList.Base) (α:=$[Int, Nat]) _ : MyList Nat Int)
+
 #print MyList.Shape
-#print MyList.Shape.ChildT
+#print MyList.Shape.P
 
-#check TypeFun.ofCurried
 
--- instance instQpfMyListShape : MvQpf (@TypeFun.ofCurried 3 MyList.Shape) := 
---   .ofPolynomial MyList.Shape.P (
---     fun x => match x with
---     | MyList.Shape.nil => _ -- ⟨MyList.Shape.HeadT.nil, fun i y => by cases i <;> simp[MvPFunctor.B, MyList.Shape.P]⟩
---     | MyList.Shape.cons .. => _
---   ) _ _ _
+/-
+instance instQpfMyListShape : MvQpf (@TypeFun.ofCurried 3 MyList.Shape) := 
+  .ofPolynomial MyList.Shape.P (
+    fun x => match x with
+    | MyList.Shape.nil b_0 b_1 => ⟨MyList.Shape.HeadT.nil, fun i j => (
+        by 
+          fin_destr i
+           <;> simp [MyList.Shape.P, MyList.Shape.ChildT, Vec.append1] at j |-
+           <;> try fin_destr j
+          
+          {
+            match j with 
+            | 0 => exact b_0
+            | 1 => exact b_1
+          }
+    )⟩
+    | MyList.Shape.cons a_0 a_1 => ⟨MyList.Shape.HeadT.cons, fun i j => (
+        by 
+          simp[TypeVec.drop, Vec.drop, Vec.reverse, Vec.normalize, Vec.append1, 
+              TypeVec.last, DVec.last, PFin2.inv, PFin2.weaken] at a_0 a_1
+          fin_destr i
+           <;> simp [MyList.Shape.P, MyList.Shape.ChildT, Vec.append1] at j |-
+           <;> try fin_destr j;
+          
+          exact a_1;
+          exact a_0;
+    )⟩
+  ) (
+    fun ⟨head, child⟩ => match head with
+    | MyList.Shape.HeadT.nil  => MyList.Shape.nil (child 0 .fz) (child 0 $ .fs .fz)
+    | MyList.Shape.HeadT.cons => MyList.Shape.cons (child 1 .fz) (child 2 .fz)
+  ) (by 
+      intro _ x;
+      rcases x with ⟨head, child⟩;
+      cases head
+      <;> simp
+      <;> apply congrArg
+      <;> fin_destr
+      <;> rfl          
+          
+  ) (by 
+      intro _ x;
+      cases x
+      <;> rfl
+  )
   
 -- #print MyList.Shape.ChildT
 -- #check @MyList.cons
 
-#check PFin2
-#check TypeVec.append1
+#check instQpfMyListShape
 
--- def ChildT : MyList.Shape.HeadT → TypeVec 3
---   | MyList.Shape.HeadT.nil  => $[PFin2 0, PFin2 1, PFin2 0]
---   | MyList.Shape.HeadT.cons => $[PFin2 1, PFin2 0, PFin2 1]
+-/
 
--- def MyList.Shape.ChildT : MyList.Shape.HeadT → Nat
---   | MyList.Shape.HeadT.nil  => 1
---   | MyList.Shape.HeadT.cons => 1
 
 -- data MyList α where
 --   | My.nil : MyList α
@@ -271,6 +459,7 @@ data MyList α β where
 -- data MyList α where
 --   | My.nil : MyList α
 --   | My2.nil : α → MyList α → MyList α
+
 
 
 
