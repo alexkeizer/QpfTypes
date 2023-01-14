36a798a6201694f0ce4facefdca37408df7e6c6c
Alex Keizer <alex@keizer.dev>
2023-01-14 19:02:36 +0100

    Remove now-redundant info in bugs

--- Qpf/Examples/_ZA_Bugs.lean
+++ Qpf/Examples/_ZA_Bugs.lean
@@ -13,17 +13,3 @@ data HigherUniv (α : Type 2) β
 
 
 
-/-
-  Big issue for dead variables, it seems partial applications of QPFs don't automatically get
-  an instance of MvQpf
--/
-
-
--- qpf F α β γ := α
-
--- instance (F : CurriedTypeFun (n + 2)) (α : Type _) [q : MvQpf (TypeFun.ofCurried F)] : 
---     MvQpf (TypeFun.ofCurried (F α)) := 
--- by
-  
-
--- #check (inferInstance : MvQpf <| TypeFun.ofCurried (F Nat))
