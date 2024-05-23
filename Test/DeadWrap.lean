9ae743a0c848d59dedaab80f2af93aaf5decaace
Alex Keizer <alex@keizer.dev>
2024-05-23 15:53:43 +0100

    chore: make tests less noisy
    There are still some `dead variables are not fully supported` messages, but otherwise all relevant `#check`s now have a `#guard_msgs`

--- Test/DeadWrap.lean
+++ Test/DeadWrap.lean
@@ -1,23 +1,23 @@
 import Qpf
 
-set_option trace.QPF true
+-- set_option trace.QPF true
 -- set_option pp.raw true
 
 namespace Test
-  data DeadWrap (α : Type) β
-    | mk : α → DeadWrap α β
 
-  #check DeadWrap.Shape
-  #check DeadWrap.Base
-  #check DeadWrap.Uncurried
+data DeadWrap (α : Type) β
+  | mk : α → DeadWrap α β
 
-  #print DeadWrap.Shape
+/-- info: 'Test.DeadWrap.Base' does not depend on any axioms  -/
+#guard_msgs in
   #print axioms DeadWrap.Base
-  #print DeadWrap.Base
 
+/-- info: Test.DeadWrap.mk {α β : Type} (a✝ : α) : DeadWrap α β -/
+#guard_msgs in
   #check DeadWrap.mk
-  #print DeadWrap.mk
 
+/-- info: DeadWrap.Shape.qpf -/
+#guard_msgs in
   #synth MvQPF.IsPolynomial <| @TypeFun.ofCurried 3 DeadWrap.Shape
 
 end Test
