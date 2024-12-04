25859286f93cbc4c31c656b6399ec39d4b3d54b8
Alex Keizer <alex@keizer.dev>
2024-12-04 15:16:36 +0000

    Apply suggestions from code review

--- Qpf/PFunctor/Multivariate/Constructions/Prod.lean
+++ Qpf/PFunctor/Multivariate/Constructions/Prod.lean
@@ -13,8 +13,8 @@ namespace Prod
 
 open PFin2 (fz fs)
 
-def P : MvPFunctor.{u} 2
-  := .mk PUnit fun _ _ => PFin2 1
+def P : MvPFunctor.{u} 2 := 
+  .mk PUnit fun _ _ => PFin2 1
 
 
 abbrev QpfProd' := P.Obj
@@ -30,8 +30,8 @@ abbrev Prod' : TypeFun 2
 /--
   Constructor for `QpfProd'`
 -/
-def mk (a : Γ 1) (b : Γ 0) : QpfProd'.{u} Γ
-  := ⟨
+def mk (a : Γ 1) (b : Γ 0) : QpfProd'.{u} Γ := 
+  ⟨
       ⟨⟩,
       fun
       | 1, _ => a
