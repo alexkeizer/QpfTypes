583858e740e80ec8266344834440e8bc41585990
Alex Keizer <alex@keizer.dev>
2024-12-18 14:37:42 +0000

    formatting of Sum.lean

--- Qpf/PFunctor/Multivariate/Constructions/Sum.lean
+++ Qpf/PFunctor/Multivariate/Constructions/Sum.lean
@@ -12,29 +12,25 @@ namespace Sum
 
 universe u
 
-def SumPFunctor : MvPFunctor.{u} 2
-  := ⟨PFin2 2,
-      fun
-      | 0 => !![PFin2 1, PFin2 0] -- inl
-      | 1 => !![PFin2 0, PFin2 1] -- inr
-    ⟩
-
+def SumPFunctor : MvPFunctor.{u} 2 :=
+  ⟨PFin2 2, fun
+    | 0 => !![PFin2 1, PFin2 0] -- inl
+    | 1 => !![PFin2 0, PFin2 1] -- inr
+  ⟩
 
 abbrev QpfSum' := SumPFunctor.Obj
 abbrev QpfSum  := TypeFun.curried QpfSum'
 
-def inl {Γ : TypeVec 2} (a : Γ 1) : QpfSum' Γ
-  := ⟨PFin2.ofNat' 0,
-      fun
-      | 1, _ => a
-    ⟩
+def inl {Γ : TypeVec 2} (a : Γ 1) : QpfSum' Γ :=
+  ⟨PFin2.ofNat' 0, fun
+    | 1, _ => a
+  ⟩
 
-def inr {Γ : TypeVec 2} (b : Γ 0) : QpfSum' Γ
-  := ⟨PFin2.ofNat' 1,
-      fun
-      | 0, _ => b
-      | 1, x => by cases x
-    ⟩
+def inr {Γ : TypeVec 2} (b : Γ 0) : QpfSum' Γ :=
+  ⟨PFin2.ofNat' 1, fun
+    | 0, _ => b
+    | 1, (x : PFin2 _) => by cases x
+  ⟩
 
 
 def Sum' := @TypeFun.ofCurried 2 Sum
